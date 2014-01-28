1b8df4686cc0a0ae647b0572a9960be198601f89
Casper Petersen <casper.kp@hotmail.com>
2014-01-28 15:21:26 +0100

    app start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExcelSchoolApp::Application.config.secret_key_base = '37bdc61d23da99506fa5f37ad863521b111378c2f5a810c83c32435abfd1cdfac650e2cc8c62b65a9c33c251d9af5c268ab77530cd7b0706113d680d207dedb8'
